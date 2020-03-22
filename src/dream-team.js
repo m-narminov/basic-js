3de9014060a5e99f7982feabcdea0a81c03d4cc2
m.narminov <m.narminov@gmail.com>
2020-03-22 21:32:44 +0300

    feat: implement dream team func

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,14 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+
+  let res = [];
+
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === 'string') {
+      let trimmed = members[i].trim();
+      res.push(trimmed[0].toUpperCase());
+    }
+  }
+
+  return res.sort().join('');
 };
